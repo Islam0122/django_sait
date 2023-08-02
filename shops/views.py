f76c9bf382f1dd02c23c0a4b6ec953cc10dec86c
islam duishobaev <dujsobaevislam01@gmail.com>
2023-08-02 23:18:31 +0600

    Initial commit

--- shops/views.py
+++ shops/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, HttpResponse,redirect
+from django.db.models import Q
+from django.shortcuts import render, HttpResponse, redirect
 import datetime
 # Create your views here.
-from shops.models import Product, Category ,Comment
-from shops.forms import Category_create,Product_create ,CommentsCreateForm
+from shops.constants import PAGINATION_LIMIT
+from shops.models import Product, Category, Comment
+from shops.forms import Category_create, Product_create, CommentsCreateForm
 
 
 def main_view(request):
@@ -12,18 +14,42 @@ def main_view(request):
 
 def shops_view(request):
     if request.method == 'GET':
-        Shops = Product.objects.all()
-        context_data = {'shops': Shops,
-                        'user': request.user}
-        return render(request, 'shops/shops.html', context=context_data)
+
+        shops = Product.objects.all()
+        search_text = request.GET.get("search")
+        page = int(request.GET.get('page', 1))
+        if search_text:
+            shops = shops.filter(Q(title__icontains=search_text) |
+                                 Q(description__icontains=search_text)
+                                 )
+
+        max_page = shops.__len__() / PAGINATION_LIMIT
+        max_page = round(max_page) + 1 if round(max_page) < max_page else round(max_page)
+
+        shops = shops[PAGINATION_LIMIT * (page - 1):PAGINATION_LIMIT * page]
+
+        context_data = {
+            'shops': shops,
+            "user": request.user,
+            'pages': range(1, max_page + 1)
+        }
+    return render(request, 'shops/shops.html', context=context_data)
 
 
 def category_view(request):
     if request.method == 'GET':
         category = Category.objects.all()
+
+        search_text = request.GET.get("search")
+
+        if search_text:
+            category = category.filter(title__icontains=search_text)
+
         context_data = {'category': category}
         return render(request, 'shops/catergory/catergories.html', context=context_data)
-def shops_d_view (request , id ):
+
+
+def shops_d_view(request, id):
     if request.method == 'GET':
         product = Product.objects.get(id=id)
 
@@ -55,6 +81,8 @@ def shops_d_view (request , id ):
         }
 
         return render(request, 'shops/detail.html', context=context)
+
+
 def create_product_view(request):
     if request.method == 'GET':
         context_data = {'form': Product_create}
